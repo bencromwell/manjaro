81da0cd1069b0d722546f22efc15733f53d1d822
Ben Cromwell <git@cromwell.dev>
2024-06-08 11:41:42 +0100

    feat: switch to chezmoi for dotfiles
    Signed-off-by: Ben Cromwell <ben@bencromwell.com>

--- install-3-config.sh
+++ install-3-config.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-gh repo clone bencromwell/dotfiles ~/projects/personal/dotfiles
-
-cp -R ~/projects/personal/dotfiles/.* ~/
+chezmoi init --apply git@github.com:bencromwell/dotfiles.git
 
 gh repo clone bencromwell/ssh-config ~/projects/ssh-config
 
