65f0bd4ae14dc003a9df4eaf0f600126aacfe514
Ben Cromwell <683855+bencromwell@users.noreply.github.com>
2024-06-08 12:34:36 +0100

    feat: set Guake prefs

--- install-3-config.sh
+++ install-3-config.sh
@@ -2,6 +2,8 @@
 
 chezmoi init --apply git@github.com:bencromwell/dotfiles.git
 
+guake --restore-preferences ~/.guakeprefs
+
 gh repo clone bencromwell/ssh-config ~/projects/ssh-config
 
 mkdir -p ~/.local/bin
